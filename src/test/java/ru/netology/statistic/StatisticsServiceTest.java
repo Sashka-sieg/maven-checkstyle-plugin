deb52c426b32f3dc0a234ea9b1f53829bee9c1f0
Yakushin Alexandr <valensiec08@mail.ru>
2021-11-03 09:10:35 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -27,4 +27,15 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+  @Test
+  void findMaxFromBillionsMiddle() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {1, 5, 8, 4, 5, 13, 8, 6, 11, 11, 12};
+    long expected = 13;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
 }
